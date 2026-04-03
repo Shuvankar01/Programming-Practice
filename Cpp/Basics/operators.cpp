112d6ffb8aba74f2ee80c6ad07bf04a68577c38e
Shuvankar01 <shuvankar.sahoo.001@gmail.com>
2026-04-03 19:39:47 +0530

    use of basic operators

---
+++ Cpp/Basics/operators.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+
+    // Basic Arithmetic Operations
+    cout << "\n--- Basic Operations ---\n";
+    cout << "Addition (+): " << a + b << endl;
+    cout << "Subtraction (-): " << a - b << endl;
+    cout << "Multiplication (*): " << a * b << endl;
+    cout << "Division (/): " << a / b << endl;
+    cout << "Modulus (%): " << a % b << endl;
+
+    return 0;
+}
