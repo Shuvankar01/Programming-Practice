276aa81332d5f3e2e96847f59fc614d06ad5b4b3
Shuvankar01 <shuvankar.sahoo.001@gmail.com>
2026-04-08 19:04:59 +0530

    Abstraction in oop

---
+++ Python/OOP/Abstraction.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+class Rectangle(Shape):
+    def __init__(self, l, w):
+        self.l = l
+        self.w = w
+
+    def area(self):
+        return self.l * self.w
+
+r = Rectangle(5, 3)
+print("Area:", r.area())
