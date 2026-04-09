4c8e2af43ffd9d897becd19624343ca5ba795c98
Shuvankar01 <shuvankar.sahoo.001@gmail.com>
2026-04-09 19:41:04 +0530

    Linear search in list

---
+++ Python/Functions/Linear_Search.py
@@ -0,0 +1,15 @@
+def linear_search(arr, key):
+    for i in range(len(arr)):
+        if arr[i] == key:
+            return i
+    return -1
+
+arr = list(map(int, input().split()))
+key = int(input())
+
+result = linear_search(arr, key)
+
+if result != -1:
+    print("Found at index", result)
+else:
+    print("Not Found")
