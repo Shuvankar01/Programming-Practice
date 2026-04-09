e167a9bb630857d6ee8188cc2d2ccb8c3df0d4b3
Shuvankar01 <shuvankar.sahoo.001@gmail.com>
2026-04-09 19:34:36 +0530

    Prime number

---
+++ Python/Functions/Prime.py
@@ -0,0 +1,14 @@
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, n // 2 + 1):
+        if n % i == 0:
+            return False
+    return True
+
+n = int(input())
+
+if is_prime(n):
+    print("Prime")
+else:
+    print("Not Prime")
