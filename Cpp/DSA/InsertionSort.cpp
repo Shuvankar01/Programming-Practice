b4b6b17ffd07d362fd1df049a0c69bc0d2d73b2d
Shuvankar01 <shuvankar.sahoo.001@gmail.com>
2026-04-17 19:22:23 +0530

    Insertion sort

---
+++ Cpp/DSA/InsertionSort.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout<<"Enter the size of an array:";
+    cin >> n;
+
+    int arr[n];
+    cout<<"Enter the elements to an array:";
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    for(int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+
+        while(j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = key;
+    }
+
+    for(int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+}
