b28e95521ac58c4ad12510be9aea6bec202d2103
Shuvankar01 <shuvankar.sahoo.001@gmail.com>
2026-04-18 19:53:54 +0530

    Binary Tree

---
+++ C/DSA/Tree.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *left, *right;
+};
+
+struct Node* createNode(int value) {
+    struct Node* newNode = malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+void inorder(struct Node* root) {
+    if(root == NULL) return;
+
+    inorder(root->left);
+    printf("%d ", root->data);
+    inorder(root->right);
+}
+
+int main() {
+    struct Node* root = createNode(1);
+    root->left = createNode(2);
+    root->right = createNode(3);
+
+    inorder(root);
+
+    return 0;
+}
