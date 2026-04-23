896caaecfde7baf786286bfeab24381fa8ccc922
Shuvankar01 <shuvankar.sahoo.001@gmail.com>
2026-04-23 16:50:25 +0530

    Second Largest Element

---
+++ Cpp/Array/second_largest.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter array elements:\n";
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    int first = arr[0], second = arr[0];
+
+    for(int i = 1; i < n; i++) {
+        if(arr[i] > first) {
+            second = first;
+            first = arr[i];
+        }
+        else if(arr[i] > second && arr[i] != first) {
+            second = arr[i];
+        }
+    }
+
+    cout << "Second Largest = " << second;
+
+    return 0;
+}
