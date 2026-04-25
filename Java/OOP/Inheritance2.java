d3693fa93057464004d63237d9e814c876d97b06
Shuvankar01 <shuvankar.sahoo.001@gmail.com>
2026-04-25 17:00:06 +0530

    Hierarchical Inheritance

---
+++ Java/OOP/Inheritance2.java
@@ -0,0 +1,29 @@
+class Animal {
+    void sound() {
+        System.out.println("Animal makes sound");
+    }
+}
+
+class Dog extends Animal {
+    void bark() {
+        System.out.println("Dog barks");
+    }
+}
+
+class Cat extends Animal {
+    void meow() {
+        System.out.println("Cat meows");
+    }
+}
+
+public class Inheritance2 {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.sound();
+        d.bark();
+
+        Cat c = new Cat();
+        c.sound();
+        c.meow();
+    }
+}
