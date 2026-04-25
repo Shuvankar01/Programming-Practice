14df97a5a66166eb32c168fe40fcffccb4e3e470
Shuvankar01 <shuvankar.sahoo.001@gmail.com>
2026-04-25 16:46:24 +0530

    Default Constructor

---
+++ Java/OOP/Constructor.java
@@ -0,0 +1,11 @@
+class Student {
+    Student() {
+        System.out.println("Default Constructor Called");
+    }
+}
+
+public class Constructor {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+    }
+}
