70d918a32ed4df0578c010af8faf8bdd3ef4d6b2
Shuvankar01 <shuvankar.sahoo.001@gmail.com>
2026-04-29 17:19:52 +0530

    Find Common Elements in Two Lists

---
+++ Python/Basics/common_elements.py
@@ -0,0 +1,6 @@
+list1 = list(map(int, input("Enter first list: ").split()))
+list2 = list(map(int, input("Enter second list: ").split()))
+
+common = list(set(list1) & set(list2))
+
+print("Common Elements =", common)
