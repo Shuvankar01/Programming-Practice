7992017d6c8186b7699e7e8e28595a789925d7a7
Shuvankar01 <shuvankar.sahoo.001@gmail.com>
2026-06-03 20:06:46 +0530

    Implemented array input output using pointers

---
+++ C/Pointers/array_using_pointers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int arr[5];
+    int *ptr;
+    int i;
+
+    ptr = arr;
+
+    printf("Enter 5 elements:\n");
+
+    for(i = 0; i < 5; i++) {
+        scanf("%d", ptr + i);
+    }
+
+    printf("Array Elements:\n");
+
+    for(i = 0; i < 5; i++) {
+        printf("%d ", *(ptr + i));
+    }
+
+    return 0;
+}
