f0c159b69a45525ec0065330ff9adc43c08dbcf9
Shuvankar01 <shuvankar.sahoo.001@gmail.com>
2026-06-05 19:29:34 +0530

    Added writing data into file

---
+++ C/File-Handling/write_file.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(){
+
+    FILE *fp;
+
+
+    fp=fopen("data.txt","w");
+
+
+    if(fp==NULL){
+
+        printf("Error opening file");
+        return 1;
+    }
+
+
+    fprintf(fp,
+    "Welcome to C Programming");
+
+
+    fclose(fp);
+
+
+    printf("Data written successfully");
+
+
+    return 0;
+}
