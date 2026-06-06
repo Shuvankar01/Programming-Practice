e5af6a9b5a1e057aa19dd044b3b38f8fd812ab3d
Shuvankar01 <shuvankar.sahoo.001@gmail.com>
2026-06-06 20:55:30 +0530

    Added GCD using recursion

---
+++ C/DSA/Recursion/gcd_recursion.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+
+int gcd(int a,int b){
+
+    if(b==0)
+        return a;
+
+
+    return gcd(b,a%b);
+
+}
+
+
+int main(){
+
+    int a,b;
+
+
+    printf("Enter numbers: ");
+    scanf("%d %d",&a,&b);
+
+
+
+    printf("GCD = %d",gcd(a,b));
+
+
+    return 0;
+}
