df0253d26e96596708d51b6df4dfaefb12524839
Shuvankar01 <shuvankar.sahoo.001@gmail.com>
2026-06-08 19:53:53 +0530

    Added deletion at position in linked list

---
+++ C/DSA/LinkedList/delete_position.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+struct Node{
+
+int data;
+struct Node *next;
+
+};
+
+
+
+int main(){
+
+
+struct Node *head;
+struct Node *second;
+struct Node *third;
+
+
+head=malloc(sizeof(struct Node));
+second=malloc(sizeof(struct Node));
+third=malloc(sizeof(struct Node));
+
+
+head->data=10;
+head->next=second;
+
+
+second->data=20;
+second->next=third;
+
+
+third->data=30;
+third->next=NULL;
+
+
+
+struct Node *temp=head;
+
+
+temp->next=temp->next->next;
+
+
+
+printf("%d ",head->data);
+
+printf("%d ",head->next->data);
+
+
+
+return 0;
+
+}
