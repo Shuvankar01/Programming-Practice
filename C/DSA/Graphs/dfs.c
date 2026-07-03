a3df94dd1697728d8011c1156b892950eda53b0a
Shuvankar01 <shuvankar.sahoo.001@gmail.com>
2026-07-03 21:15:52 +0530

    Implemented Depth First Search traversal in graph

---
+++ C/DSA/Graphs/dfs.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 10
+
+// Node for adjacency list
+struct Node {
+    int vertex;
+    struct Node* next;
+};
+
+// Graph structure
+struct Graph {
+    int numVertices;
+    struct Node* adjLists[MAX];
+    int visited[MAX];
+};
+
+// Create node
+struct Node* createNode(int v) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->vertex = v;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Create graph
+struct Graph* createGraph(int vertices) {
+    struct Graph* graph = (struct Graph*)malloc(sizeof(struct Graph));
+    graph->numVertices = vertices;
+
+    for (int i = 0; i < vertices; i++) {
+        graph->adjLists[i] = NULL;
+        graph->visited[i] = 0;
+    }
+
+    return graph;
+}
+
+// Add edge
+void addEdge(struct Graph* graph, int src, int dest) {
+    struct Node* newNode = createNode(dest);
+    newNode->next = graph->adjLists[src];
+    graph->adjLists[src] = newNode;
+
+    newNode = createNode(src);
+    newNode->next = graph->adjLists[dest];
+    graph->adjLists[dest] = newNode;
+}
+
+// DFS function
+void dfs(struct Graph* graph, int vertex) {
+    graph->visited[vertex] = 1;
+    printf("%d ", vertex);
+
+    struct Node* temp = graph->adjLists[vertex];
+
+    while (temp) {
+        int adjVertex = temp->vertex;
+
+        if (graph->visited[adjVertex] == 0) {
+            dfs(graph, adjVertex);
+        }
+
+        temp = temp->next;
+    }
+}
+
+int main() {
+    struct Graph* graph = createGraph(4);
+
+    addEdge(graph, 0, 1);
+    addEdge(graph, 0, 2);
+    addEdge(graph, 1, 3);
+    addEdge(graph, 2, 3);
+
+    printf("DFS Traversal: ");
+    dfs(graph, 0);
+
+    return 0;
+}
