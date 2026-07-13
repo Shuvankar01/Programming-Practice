bfe2b5eebd09bbd2b21a1c8b6095ec7e14a43d89
Shuvankar01 <shuvankar.sahoo.001@gmail.com>
2026-07-13 19:50:29 +0530

    Demonstrating basic While loop

---
+++ Cpp/Loops/WhileLoop.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i = 1;
+
+    cout << "Numbers from 1 to 10:\n";
+
+    while (i <= 10)
+    {
+        cout << i << " ";
+        i++;
+    }
+
+    cout << endl;
+
+    return 0;
+}
