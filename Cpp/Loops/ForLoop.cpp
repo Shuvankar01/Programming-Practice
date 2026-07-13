e9d8255f4d4966d908963b290156cd6ba14ebf43
Shuvankar01 <shuvankar.sahoo.001@gmail.com>
2026-07-13 19:47:41 +0530

    Demonstrating basic for loop

---
+++ Cpp/Loops/ForLoop.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Numbers from 1 to 10:\n";
+
+    for (int i = 1; i <= 10; i++)
+    {
+        cout << i << " ";
+    }
+
+    cout << endl;
+
+    return 0;
+}
