4fb1d99c84b6ef9e3b378f269f6b2e2f7892255c
Shuvankar01 <shuvankar.sahoo.001@gmail.com>
2026-07-13 19:54:35 +0530

    Factorial of a positive number  using for loop

---
+++ Cpp/Loops/Factorial.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int number;
+    long long factorial = 1;
+
+    cout << "Enter a positive integer: ";
+    cin >> number;
+
+    if (number < 0)
+    {
+        cout << "Factorial is not defined for negative numbers.";
+    }
+    else
+    {
+        for (int i = 1; i <= number; i++)
+        {
+            factorial *= i;
+        }
+
+        cout << "Factorial of " << number << " = " << factorial;
+    }
+
+    return 0;
+}
