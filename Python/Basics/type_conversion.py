dbace0b7ee43686b4ca347f041d7921d573fd6db
Shuvankar01 <shuvankar.sahoo.001@gmail.com>
2026-08-28 20:41:12 +0530

    feat(python): add type conversion examples

---
+++ Python/Basics/type_conversion.py
@@ -0,0 +1,15 @@
+# Type conversion in Python
+
+number = "100"
+
+integer_value = int(number)
+float_value = float(number)
+string_value = str(integer_value)
+
+print(integer_value)
+print(float_value)
+print(string_value)
+
+print(type(integer_value))
+print(type(float_value))
+print(type(string_value))
