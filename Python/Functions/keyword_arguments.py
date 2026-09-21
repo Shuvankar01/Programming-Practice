902b7c93fd5e5e282052dbbb2666067307adf862
Shuvankar01 <shuvankar.sahoo.001@gmail.com>
2026-09-21 18:36:02 +0530

    feat(python): demonstrate keyword arguments

---
+++ Python/Functions/keyword_arguments.py
@@ -0,0 +1,14 @@
+# Keyword arguments
+
+
+def student_info(name, age, course):
+    print(f"Name: {name}")
+    print(f"Age: {age}")
+    print(f"Course: {course}")
+
+
+student_info(
+    course="Computer Science",
+    name="Shuvankar",
+    age=20
+)
